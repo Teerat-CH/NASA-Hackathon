edf654a8f19efa559406a5bc65d196ea36a05a8a
Teerat-CH <teerat.nahm@gmail.com>
2023-10-08 19:50:48 -0400

    fix error5

--- app.py
+++ app.py
@@ -4,19 +4,29 @@ import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
-
-st.header("Kp Index Forecasting Model")
+st.header("ML Forecasting App")
+st.text_input("Enter your Name: ", key="name")
 
 # Load dataset and model
-data = pd.read_csv("Final_Data_for_Model_training.csv")
+data = pd.read_csv("C:/Users/Ibrah/PycharmProjects/flaskProject/Final_Data_for_Model_training.csv")
+data['Timestamp'] = pd.to_datetime(data['Timestamp']).dt.date  # Convert to date
+
 model = tf.keras.models.load_model('DSCOVR')
-#split the data
+
+# Split the data
 features = data.iloc[:, 2:].values
 target = data['Kp'].values
-X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
-#reshape the data
-X_train_reshaped = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
-X_test_reshaped = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+
+X_train, X_test, y_train, y_test = train_test_split(data.drop('Kp', axis=1), data['Kp'], test_size=0.2, random_state=42)
+test_dates = X_test['Timestamp'].values
+
+# Extract only the features without timestamp for the LSTM model
+X_train_features = X_train.drop('Timestamp', axis=1).values
+X_test_features = X_test.drop('Timestamp', axis=1).values
+
+# Reshape the data
+X_train_reshaped = X_train_features.reshape(X_train_features.shape[0], X_train_features.shape[1], 1)
+X_test_reshaped = X_test_features.reshape(X_test_features.shape[0], X_test_features.shape[1], 1)
 
 if st.checkbox('Show Dataset'):
     st.write(data)
@@ -37,12 +47,13 @@ if st.button('Make Prediction'):
     # Display the specific KP prediction for the selected time
     st.write(f"Predicted KP Index for {selected_date} is: {prediction[0][0]:.2f}")
 
-    st.write(f"Thank you! I hope you found this useful.")
+    st.write(f"Thank you {st.session_state.name}! I hope you found this useful.")
 
+    
 # Display the graph
-y_pred_all = model.predict(X_test_reshaped)  # Assuming X_test_reshaped is available
+y_pred_all = model.predict(X_test_reshaped)
 chart_data = pd.DataFrame({
-    'Real KP Values': y_test,  # Assuming y_test is globally available
+    'Real KP Values': y_test.values,
     'Predicted KP Values': y_pred_all.flatten()
 })
 st.line_chart(chart_data)
